ea2297026cf57b9a4003413f5ce04434e8be9960
Bunyamin Yildirim <bunyamin.gs@gmail.com>
2019-02-20 13:40:55 +0100

    update script

--- runLighthouse.sh
+++ runLighthouse.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Usage examples
+
 # clean
 rm -rf lighthouse
 
 # create output folder
 install -d -m 0777 lighthouse
 
-# chown -R $(id -u):$(id -g) lighthouse
-docker run -it -v $(pwd)/lighthouse:/home/app/lighthouse imageim https://google.com
+
+# uses default entrypoint flags
+# docker run -it -v $(pwd)/lighthouse:/home/app/lighthouse imageim https://google.com
+
+# with custom entrypoint flags
+sh 'docker run -it --entrypoint="" -v $PWD/lighthouse:/home/app/lighthouse bunyamin92/lightdock lighthouse --chrome-flags="--headless --no-sandbox" --output="html" --output-path="/home/app/lighthouse/report.html" https://google.com'
+
